271c39641d7b753923b926d20e0a3c113e0d3d95
mkilian <kilian.moun@ynov.com>
2023-12-12 15:45:16 +0100

    starting a server

---
+++ hangmanWebFunc/server.go
@@ -0,0 +1,22 @@
+package hangmanWeb
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func Server() {
+	http.HandleFunc("/", Home)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+}
