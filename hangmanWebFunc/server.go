80cb50036603f9cb22ca5dd4efdba0f21c0a72bf
mkilian <kilian.moun@ynov.com>
2023-12-12 18:32:02 +0100

    add my hangman package

--- hangmanWebFunc/server.go
+++ hangmanWebFunc/server.go
@@ -1,21 +1,43 @@
 package hangmanWeb
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/MounKilian/hangman"
 )
 
+type Informations struct {
+	Email   string
+	Subject string
+	Message string
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./index.html")
+	template, err := template.ParseFiles("./index.html", "./templates/footer.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	template.Execute(w, nil)
 }
 
+func Infos(w http.ResponseWriter, r *http.Request, infos *Informations) {
+	template, err := template.ParseFiles("./pages/infos.html", "./templates/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, infos)
+}
+
 func Server() {
+	fmt.Print(hangman.RandomWordUnderscore("portemanteau"))
+	infos := &Informations{"kilianmoun@gmail.com", "sujet message", "message"}
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/infos", func(w http.ResponseWriter, r *http.Request) {
+		Infos(w, r, infos)
+	})
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
