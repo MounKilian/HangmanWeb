41bc2e8ddcff3649b2336dab18966538755c854d
MOUN Kilian <mkilian@noreply.ytrack.learn.ynov.com>
2024-01-10 11:18:56 +0000

    Update 'hangmanWebFunc/game.go'

--- hangmanWebFunc/game.go
+++ hangmanWebFunc/game.go
@@ -1,126 +1,169 @@
 package hangmanWeb
 
 import (
-	"crypto/sha256"
-	"fmt"
+	"html/template"
 	"log"
-	"strconv"
+	"net/http"
 
 	"github.com/MounKilian/hangman"
 )
 
-// Init the data for play
-func InitGame(H *hangman.HangManData) {
-	if H.Point == 0 {
-		H.Loose = 0
-		H.Win = 0
-		DetectLevel(H)
-		H.NewScore = []string{H.Username, strconv.Itoa(H.Point), H.Level, strconv.Itoa(H.Win), strconv.Itoa(H.Loose)}
-		Write(H)
+func Form(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	if GameLoop(H) == 1 {
+		if H.Level == "easy" {
+			H.Point += 1 * H.Attempts
+			H.Win += 1
+		} else if H.Level == "medium" {
+			H.Point += 2 * H.Attempts
+			H.Win += 1
+		} else {
+			H.Point += 3 * H.Attempts
+			H.Win += 1
+		}
+		Update(H)
 		Read(H)
 		Refresh(H)
+		http.Redirect(w, r, "/win", http.StatusFound)
+	} else if GameLoop(H) == 0 {
+		H.Loose += 1
+		Update(H)
+		Read(H)
+		Refresh(H)
+		http.Redirect(w, r, "/loose", http.StatusFound)
 	}
-	H.ToFind = hangman.RandomWord(string(("dic/" + H.WordFile)))
-	H.Word = hangman.RandomWordUnderscore(H.ToFind)
-	H.LetterInput = ""
-	H.Attempts = 10
-	hangman.FirstLetter(H)
+	template, err := template.ParseFiles("./pages/game.html", "./templates/informations.html", "./templates/stickman.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, H)
 }
 
-// When user input smthg make some verification
-func GameLoop(H *hangman.HangManData) int {
-	if !hangman.VerifIfAlreadyUse(H) && (H.LetterInput >= "a" && H.LetterInput <= "z") {
-		H.Letters += H.LetterInput + " | "
-		if len(H.LetterInput) == 1 {
-			hangman.Verification(H)
-			if hangman.WordFind(H) {
-				return 1
-			}
-		} else if len(H.LetterInput) > 1 {
-			win := hangman.EnterWord(H)
-			if win {
-				return 1
-			}
-		}
+func GameBack(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	H.LetterInput = r.FormValue("Text input")
+	http.Redirect(w, r, "/game", http.StatusFound)
+}
+
+func Menu(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html", "./templates/header.html", "./templates/footer.html")
+	if err != nil {
+		log.Fatal(err)
 	}
-	if H.Attempts <= 0 {
-		return 0
+	template.Execute(w, nil)
+}
+
+func Help(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./pages/help.html", "./templates/header.html", "./templates/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func Win(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	template, err := template.ParseFiles("./pages/win.html", "./templates/ranking.html", "./templates/header.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, H)
+}
+
+func Loose(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	template, err := template.ParseFiles("./pages/loose.html", "./templates/ranking.html", "./templates/header.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, H)
+}
+
+func Level(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./pages/level.html", "./templates/header.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func EasyGame(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	H.WordFile = "words.txt"
+	InitGame(H)
+	http.Redirect(w, r, "/game", http.StatusFound)
+}
+
+func MediumGame(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	H.WordFile = "words2.txt"
+	InitGame(H)
+	http.Redirect(w, r, "/game", http.StatusFound)
+}
+
+func HardGame(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	H.WordFile = "words3.txt"
+	InitGame(H)
+	http.Redirect(w, r, "/game", http.StatusFound)
+}
+
+func Login(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	template, err := template.ParseFiles("./pages/login.html", "./templates/header.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, H)
+}
+
+func Change(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	H.Attempts = 0
+	if H.TypeOfGame {
+		H.TypeOfGame = false
 	} else {
-		return 3
+		H.TypeOfGame = true
 	}
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
-// Detect the level with the word file
-func DetectLevel(H *hangman.HangManData) {
-	if H.WordFile == "words.txt" {
-		H.Level = "easy"
-	} else if H.WordFile == "words2.txt" {
-		H.Level = "medium"
-	} else if H.WordFile == "words3.txt" {
-		H.Level = "hard"
+func Scoreboard(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	Read(H)
+	Refresh(H)
+	template, err := template.ParseFiles("./pages/scoreboard.html", "./templates/ranking.html", "./templates/header.html")
+	if err != nil {
+		log.Fatal(err)
 	}
+	template.Execute(w, H)
 }
 
-// Detect the word file with the level
-func DetectFile(H *hangman.HangManData) {
-	if H.Level == "easy" {
-		H.WordFile = "words.txt"
-	} else if H.Level == "medium" {
-		H.WordFile = "words2.txt"
-	} else if H.Level == "hard" {
-		H.WordFile = "words3.txt"
-	}
-}
-
-// Verification if Email or Username is already use (sign in)
-func Email(H []string) bool {
-	AllAccount := ReadSignIn()
-	for _, Account := range AllAccount {
-		if Account[1] == H[1] || Account[0] == H[0] {
-			return false
+func Username(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	if !H.TypeOfGame {
+		H.Username = r.FormValue("SignInUsername")
+		H.Password = r.FormValue("SignInPassword")
+		H.Password = hashPassword(H.Password)
+		H.Email = r.FormValue("SignInEmail")
+		H.Point = 0
+		Account := []string{H.Username, H.Email, H.Password}
+		if Email(Account) {
+			AllAccount := ReadSignIn()
+			Save(AllAccount, Account)
+			http.Redirect(w, r, "/level", http.StatusFound)
+		} else {
+			H.Attempts = 1
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+	} else {
+		H.Password = r.FormValue("LogInPassword")
+		H.Password = hashPassword(H.Password)
+		H.Email = r.FormValue("LogInEmail")
+		Account := []string{H.Username, H.Email, H.Password}
+		if AcccountUse(Account, H) {
+			Log(H)
+			Update(H)
+			Read(H)
+			Refresh(H)
+			H.ToFind = hangman.RandomWord(string(("dic/" + H.WordFile)))
+			H.Word = hangman.RandomWordUnderscore(H.ToFind)
+			H.LetterInput = ""
+			H.Attempts = 10
+			hangman.FirstLetter(H)
+			http.Redirect(w, r, "/game", http.StatusFound)
+		} else {
+			H.Attempts = 2
+			http.Redirect(w, r, "/login", http.StatusFound)
 		}
 	}
-	return true
-}
-
-// Verification if Email and Password are correct (log in)
-func AcccountUse(Account []string, H *hangman.HangManData) bool {
-	AllAccount := ReadSignIn()
-	for _, Acc := range AllAccount {
-		if Acc[1] == Account[1] && Account[2] == Acc[2] {
-			H.Username = Acc[0]
-			return true
-		}
-	}
-	return false
-}
-
-// data for start game when someone log in
-func Log(H *hangman.HangManData) {
-	for _, rec := range H.Scoreboard {
-		if H.Username == rec[0] {
-			var err error
-			H.Point, err = strconv.Atoi(rec[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-			H.Win, err = strconv.Atoi(rec[3])
-			if err != nil {
-				log.Fatal(err)
-			}
-			H.Loose, err = strconv.Atoi(rec[4])
-			if err != nil {
-				log.Fatal(err)
-			}
-			H.Level = rec[2]
-			DetectFile(H)
-			H.NewScore = []string{H.Username, strconv.Itoa(H.Point), H.Level, strconv.Itoa(H.Win), strconv.Itoa(H.Loose)}
-		}
-	}
-}
-
-// encrypt password
-func hashPassword(password string) string {
-	hash := sha256.Sum256([]byte(password))
-	return fmt.Sprintf("%x", hash)
 }
